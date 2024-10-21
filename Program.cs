ec80a8a29d86ec392fcd208e991a41b0816111c2
Lipenaminha <zfamozinz@hotmail.com>
2024-10-21 00:10:20 -0300

    MeusProjetos

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+class SenhaFixa {
+
+
+ static void Main(string[] args){
+
+ 
+
+    
+    }
+
+
+
+
+    }
